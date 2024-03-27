3bc62b91a4277d3c06b0f02876450df58ff7859c
Matthew James Collins <modish0161@gmail.com>
2024-03-27 20:40:19 +0000

    Changed around to get right 2040

--- api/index.py
+++ api/index.py
@@ -1,9 +1,28 @@
-from http.server import BaseHTTPRequestHandler
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 
-class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
-        return
+# Import your blockchain classes and functions
+from ZeroBlockchain_Prototype2_main import ZeroBlockchain
+
+app = Flask(__name__)
+CORS(app)
+
+# Create an instance of the ZeroBlockchain
+blch = ZeroBlockchain()
+
+# Define a route to return the blockchain data
+@app.route("/api/blockchain", methods=['GET'])
+def get_blockchain():
+    blockchain_data = [block.__dict__ for block in blch.chain]
+    return jsonify(blockchain_data)
+
+# Define a route to add a new block to the blockchain
+@app.route("/api/add_block", methods=['POST'])
+def add_block():
+    data = request.json.get('data') if request.json else None
+    if data is None:
+        return jsonify({'message': 'No data provided'}), 400
+    blch.add_block(data)
+    return jsonify({'message': 'Block added successfully'}), 200
+
+
